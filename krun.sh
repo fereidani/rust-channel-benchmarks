3716d7d69424a3495c99d57e6590e7eb4af4619e
fereidani <info@fereidani.com>
2022-07-15 22:19:59 +0430

    fix path for kanal only run

--- krun.sh
+++ krun.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 IFS=$'\n\t'
 cd "$(dirname "$0")"
 
-cargo run --release --bin kanal | tee kanal.csv
-cargo run --release --bin kanal-async | tee kanal-async.csv
+cargo run --release --bin kanal | tee target/kanal.csv
+cargo run --release --bin kanal-async | tee target/kanal-async.csv
 
-./plot.py ./*.csv
+./plot.py ./target/*.csv
